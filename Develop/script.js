6ea9f016a35bfffb50b089d3ee2d3f9ee0c615f0
Tyler <tylersundquist3@gmail.com>
2021-08-23 14:20:21 -0600

    created random letter/number/symbol functions

--- Develop/script.js
+++ Develop/script.js
@@ -12,6 +12,24 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomSymbol());
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
