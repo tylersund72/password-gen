4af811a931f687c04daad42a684161c0917e5f8c
Tyler <tylersundquist3@gmail.com>
2022-01-04 00:00:42 -0700

    added variables and started generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,24 @@
-// variables
-var 
+// character values
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "0123456789"
+var specialCharacters = "!@#$%^&*()_+-=;':,./<>?[]{}"
 
 
 // function to generate the password
 function generatePassword() {
-
+  let passwordLength = window.prompt("How long would you like your password to be? Choose between 8 and 128.");
+  console.log(passwordLength);
+  passwordLength = parseInt(passwordLength, 10);
+  let = "";
+  
 }
+
+// variables
+var confirmUpperCase = window.confirm("Would you like upper case letters in your password?");
+var confirmLowerCase = window.confirm("Would you like lower case letters in your password?");
+var confirmNumbers = window.confirm("Would you like numbers in your password?");
+var confirmSpecialCharacters = window.confirm("Would you like special characters in your password?");
 
 
 // Get references to the #generate element
